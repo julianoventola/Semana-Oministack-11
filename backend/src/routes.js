e6dd5f8639380969874cad6f06678172eaa25478
JulianoV <ventolajuliano@gmail.com>
2020-03-25 14:54:12 -0300

    Add - Login route for ONG

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,12 @@ const { Router } = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = Router();
+
+//Session
+routes.post('/sessions', SessionController.create);
 
 // Ongs
 routes.get('/ongs', OngController.index);
