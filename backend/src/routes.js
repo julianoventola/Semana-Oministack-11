a49cff4b8b103a2e6162a21b280d51b9aa65cd00
JulianoV <ventolajuliano@gmail.com>
2020-03-25 13:57:22 -0300

    Add - Sqlite connection

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
+
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ ok: true });
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({ id, name, email, whatsapp, city, uf });
+
+  return res.json({ id });
 });
 
 module.exports = routes;
