8787e29e0298d24908a1d4496ce244da04f1db8d
JulianoV <ventolajuliano@gmail.com>
2020-03-25 23:25:09 -0300

    Add - Frontend components and Css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './global.css';
 
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div>
-      <Logon />
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
